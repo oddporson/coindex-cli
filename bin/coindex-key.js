08df97422bebfbd54d5ebc118188312b3132e46d
Porson <porsonlee@gmail.com>
2020-04-04 22:51:20 -0700

    added 2 more commands to CLI

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -5,4 +5,14 @@ program
   .description('Set API Key -- Get at https://nomics.com')
   .action(() => console.log('Hello from set'));
 
+program
+  .command('show')
+  .description('Show API Key')
+  .action(() => console.log('Hello from show'));
+
+program
+  .command('remove')
+  .description('Remove API Key')
+  .action(() => console.log('Hello from remove'));
+
 program.parse(process.argv);
