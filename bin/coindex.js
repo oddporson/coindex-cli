f0b56e8a64faf1d326aa212bf31f257b30182276
Porson <porsonlee@gmail.com>
2020-04-04 22:48:57 -0700

    learning to create a command in CLI

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
 
-console.log("Hello from coindex");
+program
+  .version(pkg.version)
+  .command('key', 'Manage API Key -- https://nomics.com')
+  .parse(process.argv);
+
+// console.log("Hello from coindex");
